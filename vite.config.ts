fea9855a16b14bc81b6a64a4e106020fc2cb8a2d
bamenendez <bamenendez@innovacion.gob.sv>
2025-06-12 23:31:48 +0000

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.GUION_ENDPOINT': JSON.stringify(process.env.GUION_ENDPOINT),
+    'process.env.PRESENTACION_ENDPOINT': JSON.stringify(process.env.PRESENTACION_ENDPOINT),
+    'process.env.ENLACES_ENDPOINT': JSON.stringify(process.env.ENLACES_ENDPOINT),
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
